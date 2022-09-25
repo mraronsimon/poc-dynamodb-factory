8472a2065cc61fd34a6ef2e7fa6f7e72ed06f0d6
MrAronSimon <aron.simon.developer@gmail.com>
2022-09-25 19:35:25 +0200

    feat: RecordType from TableConfiguration

--- libs/dynamo-doc/src/lib/types.ts
+++ libs/dynamo-doc/src/lib/types.ts
@@ -28,6 +28,26 @@ type DyDoc_TableConfiguration = {
   attributes: { [k: string]: DyDoc_ColumnConf }
 }
 
+type DyDoc_RecordType<
+  T_table extends DyDoc_TableConfiguration,
+> = {
+  [
+    k in keyof T_table['attributes']
+    as T_table['attributes'][k] extends `Required${string}` ? k : never
+  ]:
+  T_table['attributes'][k] extends `RequiredString` ? string
+      : T_table['attributes'][k] extends `RequiredNumber` ? number
+        : never;
+} & {
+  [
+    k in keyof T_table['attributes']
+    as T_table['attributes'][k] extends `Optional${string}` ? k : never
+  ]?:
+  T_table['attributes'][k] extends `OptionalString` ? string
+      : T_table['attributes'][k] extends `OptionalNumber` ? number
+        : never;
+}
+
 export type DyDoc_TableBuilder = {
   createTable<
     T_pk extends DyDoc_ColumnConfForPrimaryKey,
@@ -47,9 +67,9 @@ type DyDoc_TableBuilderAdditions<
 > = {
   addGlobalSecondaryIndex<
     T_name extends string,
-    T_pk extends DyDoc_ColumnConfForPrimaryKey,
-    T_sk extends DyDoc_ColumnConfForPrimaryKey,
-    T_keyConfiguration extends DyDoc_PrimaryKeyConfiguration<T_pk, T_sk>
+    T_pk extends DyDoc_ColumnConfForSecondaryKey,
+    T_sk extends DyDoc_ColumnConfForSecondaryKey,
+    T_keyConfiguration extends DyDoc_SecondaryKeyConfiguration<T_pk, T_sk>
   >(
     name: T_name extends T_table['gsi'][number] ? `GSI '${T_name}' already exists!` : T_name,
     config: T_keyConfiguration
@@ -65,10 +85,17 @@ type DyDoc_TableBuilderAdditions<
         : never
     }
   }>;
+
+  getConfiguration(): T_table;
 }
 
 declare const tableBuilder: DyDoc_TableBuilder;
 
 const myTable = tableBuilder
   .createTable({ pk: 'RequiredNumber', sk: 'RequiredString' })
-  .addGlobalSecondaryIndex('cica', { pk: 'RequiredNumber' })
+  .addGlobalSecondaryIndex('1', { pk: 'RequiredNumber' })
+  .addGlobalSecondaryIndex('2', { pk: 'OptionalString' })
+
+const myTableConfiguration = myTable.getConfiguration();
+
+type MyRecordType = DyDoc_RecordType<typeof myTableConfiguration>
