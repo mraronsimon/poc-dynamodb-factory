de194284ff4b72089a30b842856f68ebcd6b8e51
MrAronSimon <aron.simon.developer@gmail.com>
2022-09-26 08:55:52 +0200

    test: schemaTableBuilderFactory addGlobalSecondaryIndex

--- libs/dynamo-doc/src/lib/table-schema-builder.spec.ts
+++ libs/dynamo-doc/src/lib/table-schema-builder.spec.ts
@@ -42,4 +42,49 @@ describe('schemaTableBuilderFactory', () => {
       .createTable({ pk: 'RequiredString' })
       .finalizeTableSchema();
   });
+  it('Should create GSI_example_PK', (done) => {
+    const TableBuilder = schemaTableBuilderFactory((config) => {
+      expect(config).toEqual({
+        attributes: {
+          PK: 'RequiredString',
+          GSI_example_PK: 'OptionalNumber'
+        },
+        documents: {},
+        gsi: ['example'],
+        lsi: []
+      })
+      done();
+
+      return {
+        addDocumentType: () => { throw new Error('Not implemented') }
+      }
+    })
+    TableBuilder
+      .createTable({ pk: 'RequiredString' })
+      .addGlobalSecondaryIndex('example', { pk: 'OptionalNumber' })
+      .finalizeTableSchema();
+  });
+  it('Should create GSI_example_PK/GSI_example_SK', (done) => {
+    const TableBuilder = schemaTableBuilderFactory((config) => {
+      expect(config).toEqual({
+        attributes: {
+          PK: 'RequiredString',
+          GSI_example_PK: 'OptionalNumber',
+          GSI_example_SK: 'OptionalString'
+        },
+        documents: {},
+        gsi: ['example'],
+        lsi: []
+      })
+      done();
+
+      return {
+        addDocumentType: () => { throw new Error('Not implemented') }
+      }
+    })
+    TableBuilder
+      .createTable({ pk: 'RequiredString' })
+      .addGlobalSecondaryIndex('example', { pk: 'OptionalNumber', sk: 'OptionalString' })
+      .finalizeTableSchema();
+  });
 });
